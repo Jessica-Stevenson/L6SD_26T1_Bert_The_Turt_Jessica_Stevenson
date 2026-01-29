c12ce3de942e9ba0dc92c1881da5c2f64a89ab32
Jessica <Jessica@DESKTOP-U34H195>
2026-01-30 10:21:57 +1300

    finished head

--- Bert_Draws_My_Art.py
+++ Bert_Draws_My_Art.py
@@ -2,7 +2,7 @@ import turtle
 s = turtle.getscreen()
 Bert = turtle.Turtle()
 
-Bert.pen(pencolor="#8b1017", fillcolor="#d85d4a", pensize=10)
+Bert.pen(pencolor="#8b1017", fillcolor="#d85d4a", pensize=10, speed=80)
 
 
 Bert.begin_fill()
@@ -10,10 +10,10 @@ Bert.penup()
 Bert.goto(-150,90)
 Bert.right(50)
 Bert.pendown()
-#first Curve
+#Curve
 Bert.circle(100,-140)
 
-#little hair piece
+#Little hair piece
 Bert.left(90)
 Bert.forward(10)
 Bert.left(180)
@@ -25,14 +25,31 @@ Bert.forward(20)
 Bert.right(140)
 Bert.forward(40)
 
-#Fringe 1
+#Fringe
 Bert.right(180)
 Bert.forward(10)
 Bert.right(90)
 Bert.circle(-100,120)
 Bert.right(160)
 Bert.circle(100, 80)
+Bert.left(40)
+Bert.circle(90, 120)
+Bert.goto(-150,90)
+Bert.end_fill()
+
+#Moving bert to next part
+Bert.penup()
+Bert.left(50)
+Bert.forward(58)
+Bert.pendown()
+
+Bert.pen(pencolor="#f19a95", fillcolor="#d85d4a", pensize=10, speed=80)
 
 
+#Face
+Bert.right(20)
+Bert.forward(60)
+Bert.left(60)
+Bert.forward(50)
 
 turtle.done()
